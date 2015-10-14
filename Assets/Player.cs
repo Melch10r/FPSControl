0965c9d438a31293dcd4edb5ad2cb555459179e3
Melch10r <melchiorjong@gmail.com>
2015-10-14 13:31:27 +0200

    First check
    whazup?

---
+++ Assets/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Player : MonoBehaviour {
+
+	//store the position of the player
+	Vector3 pos;
+
+	//Use this for initialization
+	void Start () 
+	{
+		//set the position to where we start off in the scene
+		pos = transform.position;
+	}
+
+	//Update is called once per frame
+	void Update () {
+		bool WKey = Input.GetKey(KeyCode.W);
+		bool SKey = Input.GetKey(KeyCode.S);
+		bool AKey = Input.GetKey(KeyCode.A);
+		bool DKey = Input.GetKey(KeyCode.D);
+		if(WKey) {
+			pos.z += 0.1f;
+		}
+		if(SKey) {
+			pos.z -= 0.1f;
+		}
+		if(AKey) {
+			pos.x -= 0.1f;
+		}
+		if(DKey) {
+			pos.x += 0.1f;
+		}
+		gameObject.transform.position = pos;
+	}
+}
---
+++ Assets/Zombie.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Zombie : MonoBehaviour
+{
+	public static int numZombies;
+	public bool die;
+	public GameObject player;
+	public float speed = 0.01f;
+
+	//Use this for initialization
+	void Start ()
+	{
+		player = GameObject.Find ("Main Camera");
+		numZombies++;
+		Debug.Log(numZombies);
+	}
+
+	public static void CountZombies()
+
+	{
+		Debug.Log (numZombies);
+	}
+	void Update ()
+	{
+		Vector3 direction = (player.transform.position - transform.position).normalized;
+		float distance = (player.transform.position - transform.position).magnitude;
+		Vector3 move = transform.position + (direction * speed);
+		transform.position = move;
+		if (distance < 1f)
+		{
+			die = true;
+		}
+		if (die)
+		{
+			numZombies-- ;
+			Destroy(gameObject);
+		}
+	}
+}
